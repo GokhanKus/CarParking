95011936eb793776125ebd599a6bf8dabcae5955
GokhanKus <gkus1998@hotmail.com>
2023-04-05 22:57:43 +0300

    MobileInputUpdated
    Gas&Brake Pedal Added

--- Assets/Scripts/CarUserControl.cs
+++ Assets/Scripts/CarUserControl.cs
@@ -1,6 +1,8 @@
 using SimpleInputNamespace;
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 [System.Serializable]
@@ -9,7 +11,7 @@ public class AxleInfo
     public WheelCollider leftWheel;
     public WheelCollider RightWheel;
 
-    public bool motor; // is this wheel attached to motor?
+    public bool motorPower; // is this wheel attached to motor?
     public bool steering; // does this wheel apply steer angle?
 
 }
@@ -21,6 +23,10 @@ public class CarUserControl : MonoBehaviour
     public float maxSteeringAngle;
     public float brakeForce = 3000f;
     public GameObject SteerWheel;
+
+    bool isBrakePedalPressed;
+    float moveInput;
+
     public void ApplyLocalPositionToVisuals(WheelCollider collider)
     {
 
@@ -43,37 +49,88 @@ public class CarUserControl : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        float motor = maxMotorTorque * Input.GetAxis("Vertical");
-        //float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
-        float steering = maxSteeringAngle * SteerWheel.GetComponent<SteerWheel>().GetClampedValue();
 
+        //float motor = maxMotorTorque * Input.GetAxis("Vertical");
+        //float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
+        //float steering = maxSteeringAngle * SteerWheel.GetComponent<SteerWheel>().GetClampedValue();
+        Gas();
+        Brake();
+        Steering();
+
+    }
+
+    //For Mobile Input;
+    public void BrakeInputDown()
+    {
+        isBrakePedalPressed = true;
+    }
+    public void BrakeInputUp()
+    {
+        isBrakePedalPressed = false;
+    }
+    public void GasInput(float input)
+    {
+        moveInput = input;
+    }
+
+    void Gas()
+    {
+        //float motor = maxMotorTorque * Input.GetAxis("Vertical");  
         foreach (AxleInfo axleInfo in axleInfos)
         {
-            if (axleInfo.motor == true)
+            axleInfo.leftWheel.motorTorque = maxMotorTorque * moveInput;
+            axleInfo.RightWheel.motorTorque = maxMotorTorque * moveInput;
+        }
+
+        //foreach (AxleInfo axleInfo in axleInfos)
+        //{
+        //    if (axleInfo.motorPower == true)
+        //    {
+        //        axleInfo.leftWheel.motorTorque = motor;
+        //        axleInfo.RightWheel.motorTorque = motor;
+        //    }
+
+
+        //    ApplyLocalPositionToVisuals(axleInfo.leftWheel); //tekerin, wheel colliderin ile birlikte donmesini saglayan metot
+        //    ApplyLocalPositionToVisuals(axleInfo.RightWheel); //bu metodu yazmasak sadece wheel collider saga sola dönerdi, teker donmezdi.
+        //}
+    }
+    void Brake()
+    {
+        if (isBrakePedalPressed)
+        {
+            foreach (AxleInfo axleInfo in axleInfos)
             {
-                if (Input.GetKey(KeyCode.Space))
-                {
-                    axleInfo.leftWheel.brakeTorque = brakeForce;
-                    axleInfo.RightWheel.brakeTorque = brakeForce;
-                }
-                if (Input.GetKeyUp(KeyCode.Space))
-                {
-                    axleInfo.leftWheel.brakeTorque = 0;
-                    axleInfo.RightWheel.brakeTorque = 0;
-                }
-                axleInfo.leftWheel.motorTorque = motor;
-                axleInfo.RightWheel.motorTorque = motor;
+                axleInfo.leftWheel.brakeTorque = brakeForce;
+                axleInfo.RightWheel.brakeTorque = brakeForce;
             }
+        }
+        else
+        {
+            foreach (AxleInfo axleInfo in axleInfos)
+            {
+                axleInfo.leftWheel.brakeTorque = 0;
+                axleInfo.RightWheel.brakeTorque = 0;
+            }
+        }
+    }
+    void Steering()
+    {
+        float steering = maxSteeringAngle * SteerWheel.GetComponent<SteerWheel>().GetClampedValue();
+        foreach (AxleInfo axleInfo in axleInfos)
+        {
+            //if (axleInfo.motorPower == true)
+            //{
+            //    axleInfo.leftWheel.motorTorque = motor;
+            //    axleInfo.RightWheel.motorTorque = motor;
+            //}
             if (axleInfo.steering == true)
             {
                 axleInfo.leftWheel.steerAngle = steering;
                 axleInfo.RightWheel.steerAngle = steering;
             }
-
-
             ApplyLocalPositionToVisuals(axleInfo.leftWheel); //tekerin, wheel colliderin ile birlikte donmesini saglayan metot
             ApplyLocalPositionToVisuals(axleInfo.RightWheel); //bu metodu yazmasak sadece wheel collider saga sola dönerdi, teker donmezdi.
         }
-
     }
 }
